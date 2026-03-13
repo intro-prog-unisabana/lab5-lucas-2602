3d54fd83a1ea9f019d5aace72a3165d70bc5040b
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-13 18:19:26 -0500

    response.py

--- response.py
+++ response.py
@@ -1,4 +1,10 @@
-user_input = 0
-generate_value = 0
 def input_response(generate_value, user_input):
-    user_input = input()
+    while True:
+        user_input = input("What is your guess: ")
+        if user_input < generate_value:
+            print("Too low! Try a higher number.")
+        elif user_input > generate_value:
+            print("Too high! Try a lower number.")
+        else:
+            print("Correct! You guessed the number!")
+            break
