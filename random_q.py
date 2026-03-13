742e45782192bdc859a4ba7316831cf9e5f8d49e
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-13 11:08:53 -0500

    random_q.py

--- random_q.py
+++ random_q.py
@@ -1,6 +1,6 @@
 import random
-random.seed(123)
 inicio = int(input("Enter the start value:\n"))
 final = int(input("Enter the end value:\n"))
+random.seed(123)
 random_number = random.randint(inicio, final)
 print(f"Generated random number: {random_number}")
