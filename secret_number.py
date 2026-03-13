78ddf9e3e5a96385a496db30939da73361ae7d19
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-13 18:12:00 -0500

    secret_number.py

--- secret_number.py
+++ secret_number.py
@@ -1,11 +1,7 @@
 import random
-start = 0
-end = 0
-seed = 0
 def seed_secret_numbers(seed):
     random.seed(6)
-    generate_secret_number(1, 100)
-def generate_secret_number(start, end):
-    resu = random.randint(start, end)
-    return resu
-print(generate_secret_number(start, end))
+    return seed
+def generate_secret_number(start=1, end=100):
+    num = random.randint(start, end)
+    return num
