f7f34d26a3e075c07d4edc20b443490ab30281b8
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-14 16:14:11 -0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-from utils import add, sub, multiply, divide, exponent, modulo, floor_divide
+from utils import add, sub, multiply, divide, exponent, modulo, floor_divide, absolute
+while True:
+    entrada = input("Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit):\n")
+    if entrada == "add":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        add(num1, num2)
+        continue
+    elif entrada == "subtract":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        sub(num1, num2)
+        continue
+    elif entrada == "multiply":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        multiply(num1, num2)
+        continue
+    elif entrada == "divide":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        divide(num1, num2)
+        continue
+    elif entrada == "exponent":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        exponent(num1, num2)
+        continue
+    elif entrada == "modulo":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        modulo(num1, num2)
+        continue
+    elif entrada == "floor_divide":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        floor_divide(num1, num2)
+        continue
+    elif entrada == "absolute":
+        num1 = float(input("Enter the number: "))
+        absolute(num1)
+        continue
+    elif entrada == "exit":
+        break
+    else:
+        print("Invalid option!")
+        continue
