c1c44eb1c34fb370411dd6ae384ce96524d94225
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-14 22:23:17 -0500

    return_types.py

--- return_types.py
+++ return_types.py
@@ -1,6 +1,6 @@
 def obtener_precio_usuario():
     """Convierte el numero ingresado por el usuario, a un flotante"""
-    entrada = input("Enter the item's price:\n")
-    return float(entrada)
+    result = input("Enter the item's price:\n")
+    return float(result)
 precio = obtener_precio_usuario()
 print(precio)
--- test_all.py
+++ test_all.py
@@ -527,4 +527,6 @@ class TestMainCalculator:
 
 
 # FREEZE CODE END
-
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__, "-v"])
