643ac68872e47d2f7694a39492c4a14ee876039f
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-14 21:33:13 -0500

    scope.py

--- scope.py
+++ scope.py
@@ -8,6 +8,7 @@ def set_globals(some_int, some_str):
 def get_globals():
     return (int_value, str_value)
 
-print(get_globals())       # Salida: (None, None)
-set_globals(10, "Hello")
-print(get_globals())       # Salida: (10, "Hello")
+if __name__ == "__main__":
+    print(get_globals())       # Salida: (None, None)
+    set_globals(10, "Hello")
+    print(get_globals())       # Salida: (10, "Hello")
