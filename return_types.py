bd7cb1d0003c9fb10a170cce621d3bd305566aac
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-14 22:30:12 -0500

    return_types.py

--- return_types.py
+++ return_types.py
@@ -1,7 +1,7 @@
 def obtener_precio_usuario():
     """Convierte el numero ingresado por el usuario, a un flotante"""
     print("Enter the item's price:\n")
-    result = 25
+    result = input()
     return float(result)
 precio = obtener_precio_usuario()
 print(precio)
