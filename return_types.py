05cf1872ac900d14b0987bd4064fa1314cc52c34
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-03-16 09:15:52 -0500

    return_types.py

--- return_types.py
+++ return_types.py
@@ -1,5 +1,3 @@
 def obtener_precio_usuario():
     precios = float(input("Enter the item's price:\n"))
     return precios
-precio = obtener_precio_usuario()
-print(precio)  # Si el usuario ingresa 25, la salida será 25.0
